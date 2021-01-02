ede4fc7ef08a97ad6450699b9e110f6ee6bf45fa
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2021-01-02 10:25:21 +0200

    Adding Unit Variables

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 
-
-const cells = 10;
+const cellsHorizontal = 4;
+const cellsVertical = 3;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = width / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0;
