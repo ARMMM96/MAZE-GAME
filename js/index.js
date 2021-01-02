f256bb94669848a49184e3e60d9346b8e8653d88
AHMED RABIE <ahmed.rabie.mahmoud.96@gmail.com>
2021-01-02 05:30:02 +0200

    Drawing the Goal and updating the walls size

--- js/index.js
+++ js/index.js
@@ -27,10 +27,10 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 World.add(world, walls);
 
@@ -157,3 +157,17 @@ verticals.forEach((row, rowIndex) => {
         World.add(world, wall);
     });
 });
+
+const goal = Bodies.rectangle(
+
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        isStatic: true
+    }
+
+);
+
+World.add(world, goal);
